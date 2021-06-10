0b35f6fe71f04f92fdd5b19e17cc15fa85dadcf7
Esteban Zacharzewski <zacha.eg@gmail.com>
2021-06-10 15:37:50 -0300

    add TagEventProducer

--- app/model/Tag.scala
+++ app/model/Tag.scala
@@ -1,5 +1,11 @@
 package model
 
+import play.api.libs.json.Json
+
 import java.util.UUID
 
 case class Tag(id: UUID, text: String)
+
+object Tag {
+  implicit val writes = Json.writes[Tag]
+}
---
+++ app/services/TagEventProducer.scala
@@ -0,0 +1,41 @@
+package services
+
+import akka.actor.TypedActor.dispatcher
+import com.github.stzr1123.events.{EventData, LogRecord, TagCreated, TagDeleted}
+import dao.LogDao
+import model.Tag
+
+import java.time.ZonedDateTime
+import java.util.UUID
+import scala.concurrent.Future
+
+class TagEventProducer(logDao: LogDao, readService: ReadService) {
+  private def createLogRecord(eventData: EventData): LogRecord = {
+    LogRecord(UUID.randomUUID(), eventData.action, eventData.json, ZonedDateTime.now())
+  }
+
+  private def adjustReadState(event: LogRecord): Future[Seq[Tag]] = {
+    readService.adjustState(event).flatMap { _ =>
+      readService.getState.map(_.tags)
+    }
+  }
+
+  def createTag(text: String, createdBy: UUID): Future[Seq[Tag]] = {
+    val tagId = UUID.randomUUID()
+    val event = TagCreated(tagId, text, createdBy)
+    val record = createLogRecord(event)
+    // this is shit for obvious reasons. we're sending the same update
+    // to two different places
+    logDao.insertLogRecord(record).flatMap { _ =>
+      adjustReadState(record)
+    }
+  }
+
+  def deleteTag(tagId: UUID, deletedBy: UUID): Future[Seq[Tag]] = {
+    val event = TagDeleted(tagId, deletedBy)
+    val record = createLogRecord(event)
+    logDao.insertLogRecord(record).flatMap { _ =>
+      adjustReadState(record)
+    }
+  }
+}
