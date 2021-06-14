1a5c120168ea84b2327dae2d373da2664fe074cc
Esteban Zacharzewski <zacha.eg@gmail.com>
2021-06-14 13:15:16 -0300

    ui: add TagManager

--- ui/scripts/app/AppComponent.jsx
+++ ui/scripts/app/AppComponent.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import TagManager from './views/TagManager.jsx';
 
 
 class AppComponent {
@@ -21,17 +22,7 @@ class AppComponent {
   renderComponent = () => {
     const reactDiv = document.getElementById('reactDiv');
     if (reactDiv !== null) {
-      ReactDOM.render(
-        <div className="view-home-composite__react-panel__welcome-text">
-          If you can see this message then
-          the React-based part of the application was successfully connected
-          to the Play-based part.
-          <br/>
-          Now try logging in/logging out using the navigation links on the top.
-          The default user has credentials <code>user@example.com</code> and <code>password123</code>.
-          <br/>
-          You can also register a new user using the <a href="/signup">Sing Up form</a>.
-        </div>, reactDiv);
+      ReactDOM.render(<TagManager />, reactDiv);
     }
   }
 }
