04ca8b352f842973e874f7cc5a8b37999aa3fc42
Esteban Zacharzewski <zacha.eg@gmail.com>
2021-06-28 14:06:53 -0300

    ui: add SSE support

--- ui/scripts/app/AppComponent.jsx
+++ ui/scripts/app/AppComponent.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import TagManager from './views/TagManager.jsx';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 
 
 class AppComponent {
   init = () => {
     this.initLoginRedirecting();
+    this.initAppState();
+    this.connectToSSEEndpoint();
     this.renderComponent();
   };
   initLoginRedirecting = () => {
@@ -19,12 +23,51 @@ class AppComponent {
       return Promise.reject(error);
     });
   };
+
+  initAppState = () => {
+    const initialState = {
+      tags: []
+    };
+    const reducer = (state = initialState, action) => {
+      const updatedState = {...state};
+      const actionType = action.type;
+
+      if (actionType === 'tags_updated') {
+        updatedState['tags'] = action.data;
+      }
+      return updatedState;
+    };
+    this.store = createStore(reducer);
+  };
+
+  connectToSSEEndpoint = () => {
+    this.es = new EventSource('/api/sse');
+    this.es.addEventListener('message', this.onServerSideEvent);
+  };
+
+  onServerSideEvent = (event) => {
+    if (event.type === 'message') {
+      this.updateReceived(JSON.parse(event.data));
+    }
+  }
+
+  updateReceived = (data) => {
+    if (data['updateType'] === 'tags') {
+      this.store.dispatch({
+        type: 'tags_updated',
+        data: data['updateData']
+      })
+    }
+  };
+
   renderComponent = () => {
     const reactDiv = document.getElementById('reactDiv');
     if (reactDiv !== null) {
-      ReactDOM.render(<TagManager />, reactDiv);
+      ReactDOM.render(<Provider store={this.store}>
+        <TagManager />;
+      </Provider>, reactDiv);
     }
-  }
+  };
 }
 
 export default AppComponent;
